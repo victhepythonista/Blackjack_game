edea8565e701b05fc4014876ec48459ed503874e
victhepythonista <80308387+victhepythonista@users.noreply.github.com>
2022-11-20 22:47:36 +0300

    Removed  the Testcase class

--- test.py
+++ test.py
@@ -1,46 +1,58 @@
 # run this script to perform tests on the BlACKJACK GAME 
 
-import unittest
-
 
 from game import *
 
 
+ 
 
-class BlackjackGameTest(unittest.TestCase):
+def test_IsBlackJack():
+	# test if the IsBlackjack function can identify if  a 
+	# hand is blackjack... 
+	hand = [10,11] # this hand is  blackjack 
+	false_hand = [7,6] # this hand is not  blackjack
+	print("function_test :   IsBlackJack   - ", end= "") 
+
+	if IsBlackJack(hand) and not IsBlackJack(false_hand):
+		print("  ok -")
+	else:
+		print("  FAIL ")
 
 
+def test_AdjustAceValue():
+	# test if AdjustAce value function changes the value of an ace 
+	# if the total sum of the hand is more than 21 
+	print("function_test :   AdjustAceValue   - ", end= "") 
 
-	def test_DealHand(self):
-		# test the DealHand function 
-		# if ok it should add a new random card ti the hand
-		hand   = []
-		DealHand(hand)
-		self.assertIsInstance( hand , list )
+	test_hand = [11,6,9]
+	test_hand = AdjustAceValue(test_hand)
 
-
-
-	def test_IsBlackJack(self):
-		# test if the IsBlackjack function can identify if  a 
-		# hand is blackjack... 
-		hand = [10,11] # this hand is  blackjack 
-		false_hand = [7,6] # this hand is not  blackjack
-		self.assertTrue(IsBlackJack(hand))
-		self.assertFalse(IsBlackJack(false_hand))
-
-	def test_AdjustAceValue(self):
-		# test if AdjustAce value function changes the value of an ace 
-		# if the total sum of the hand is more than 21 
-		test_hand = [11,6,9]
-		test_hand = AdjustAceValue(test_hand)
-
-		target_total = 16  # this is the new sum of the hand if the ace in the deck is changed to a value of 1
-
-		self.assertEqual(sum(test_hand), target_total)
+	target_total = 16  # this is the new sum of the hand if the ace in the deck is changed to a value of 1
+	if sum(test_hand) == target_total:
+		print("  OK -")
+	else:
+		print("  FAIL -")
 
 
 
 
 
+def test_DealHand():
+	# test the DealHand function 
+	# if ok it should add a new random card ti the hand
+	hand   = []
+	DealHand(hand)
+	print("function_test :   DealHand- ", end= "") 
+	if len(hand) == 2 :
+		print("OK ")
+	else:
+		print("FAIL")
+
+def RunAllTests():
+	test_DealHand()
+	test_IsBlackJack()
+	test_AdjustAceValue()
+
+ 
 if __name__ == '__main__':
-	unittest.main()
+	RunAllTests()
